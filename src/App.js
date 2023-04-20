6abd0e6b97ff9dd3dc122aa5c44f290927e3ddb3
Pedro Paulo <pedropaulodfr@outlook.com>
2023-04-20 08:20:54 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Produtos from './pages/Produtos';
+import PrivateRoutes from './utils/PrivateRoutes';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route element={<Home />} path="/" exact />
+            <Route element={<Produtos />} path="/produtos" />
+          </Route>
+          <Route element={<Login />} path="/login" />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
